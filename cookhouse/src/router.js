dafc2cd5b9e06f750e7745538803035ea084b7c3
emmalkl <emma_lkl@163.com>
2019-04-24 19:47:10 +0800

    tset

--- cookhouse/src/router.js
+++ cookhouse/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Profile from './views/Profile.vue'
 import Detail from './views/Detail.vue'
+import Cart from './views/Cart.vue'
 
 Vue.use(Router)
 
@@ -29,6 +30,11 @@ export default new Router({
       path: '/detail',
       name: 'detail',
       component: Detail
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
     },
   ]
 })
--- cookhouse/src/views/Cart.vue
+++ cookhouse/src/views/Cart.vue
@@ -1,7 +1,33 @@
 <template>
     <div class="cart">
+        <!-- 上导航 -->
+        <van-nav-bar title="购物车" left-text="返回" left-arrow @click-left="onClickLeft">
+           
+           <van-icon name="search" slot="right" />
+        </van-nav-bar>
+       <!-- picture -->
+       <img src="http://i2.chuimg.com/4cf46b1066a843f6b8ddd05c78db529b_1920w_1080h.jpg?imageView2/1/w/490/h/260/interlace/1/q/90" alt="" width="100%" height="200px">
+       <p>名称：菠萝包</p>
+       <span style="line-height:44px">用料：</span>  <van-button plain type="primary" class="cart-add">加入购物车</van-button>
+       <div>
+           <van-list
+            finished-text="没有更多了"
+            >
+            <van-cell
+                v-for="item in list"
+                :key="item"
+                :title="item"
+            />
+            </van-list>
+       </div>
+       <!-- video -->
+
+       <p style="color:#ff976a">vip专享视频展示</p>
+       <video src="movie.ogg" controls="controls" class="cart-video">
+        
+       </video>
       <!-- 下导航 -->
-      <van-goods-action>
+      <van-goods-action class="cart-bar">
         <van-goods-action-mini-btn
             icon="chat-o"
             text="客服"
@@ -25,9 +51,47 @@
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            list:[
+                '高筋面粉	160克',
+                '低筋面粉	40克',
+                '食用盐	2克',
+                '细砂糖	35克',
+                '奶粉	6克',
+                '全蛋液	20克',
+                '食用水	110克',
+                '干酵母	3克',
+                '无盐黄油	25克',
+                '菠萝皮',
+                '无盐黄油	40克',
+                '无盐黄油	40克',
+                '全蛋液	15克',
+                '奶粉	8克',
+                '低筋面粉	65克'
+            ]
+        }
+    },
+    methods:{
+        onClickLeft() {
+         this.$router.go(-1);
+      },
+    }
 }
 </script>
 <style lang="scss" scoped>
- 
+.cart{
+    &-bar{
+        z-index: 1000;
+    }
+    &-add{
+        float: right;
+
+    }
+    &-video{
+        margin-bottom: 55px;
+        width: 100%;
+        
+    }
+}
 </style>
