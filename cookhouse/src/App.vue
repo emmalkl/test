469af6b6381a8392a74712e74250494cadbc3d69
水瓶座的木木欣 <634144330@qq.com>
2019-04-24 17:35:00 +0800

    ''

--- cookhouse/src/App.vue
+++ cookhouse/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <router-view/>
+    <div id="nav">
+      <van-tabbar v-model="active">
+        <van-tabbar-item icon="wap-home" to='/'>首页</van-tabbar-item>
+        <van-tabbar-item icon="fire-o" color="red" to='/hot'>热门</van-tabbar-item>
+        <van-tabbar-item icon="comment-o" info="5" to='/discuss'>讨论区</van-tabbar-item>
+        <van-tabbar-item icon="shopping-cart-o" to='/cart'>购物车</van-tabbar-item>
+        <van-tabbar-item icon="contact" to='/profile'>我的</van-tabbar-item>
+      </van-tabbar>
+    </div>
   </div>
 </template>
 
